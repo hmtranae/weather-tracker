7d5be82134b2621222224ab221c5e1b2ecca41dc
Hieu Tran <hmtran.ae@gmail.com>
2019-01-29 19:12:37 -0500

    hook up app.js and index.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class App extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>App</div>
+        )
+    }
+}
