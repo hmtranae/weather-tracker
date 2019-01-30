4806d208f26c9e4d7d4e944e67f45f8d07e845d4
Hieu Tran <hmtran.ae@gmail.com>
2019-01-30 18:05:25 -0500

    install chart.js for charting weather history temperature

---
+++ src/components/WeatherHistory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class WeatherHistory extends Component {
+    componentDidMount = () => {}
+
+    render() {
+        return (
+            <div>WeatherHistory</div>
+        )
+    }
+}
