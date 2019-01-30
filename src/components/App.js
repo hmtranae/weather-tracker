eca7ed758bdd87a81e17a7884740084349126170
Hieu Tran <hmtran.ae@gmail.com>
2019-01-29 19:40:20 -0500

    use navigator geolocation to grab user location

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class App extends Component {
+    state = {
+        userLocation: {
+            latitude: null,
+            longitude: null
+        }
+    }
+
     componentDidMount() {
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position)
+            const { latitude, longitude } = position.coords
+            const userLocation = {
+                latitude: latitude,
+                longitude: longitude
+            }
+            this.setState({
+                userLocation
+            })
         })
     }
 
     render() {
+        console.log(this.state.userLocation)
         return (
             <div>App</div>
         )
