4da6510a58c263cc140e1f359324d46541ace952
Hieu Tran <hmtran.ae@gmail.com>
2019-01-29 21:13:45 -0500

    successful request for input city submit

--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -1,36 +1,73 @@
 import React, { Component } from 'react'
-import axios from 'axios';
-import _ from 'lodash';
-import { API_KEY, baseUrl } from '../apis/OpenWeatherMap';
+import axios from 'axios'
+import _ from 'lodash'
+import { API_KEY, baseUrl } from '../apis/OpenWeatherMap'
 
 export default class LocationInput extends Component {
+  state = {
+    city: ''
+  }
+
   componentDidUpdate = async () => {
-      if (_.isEmpty(this.props) === false) {
-        const { latitude, longitude } = this.props.location
-        let data = await axios.get(`${baseUrl}?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`)
-        console.log(data);
-      }
+    if (_.isEmpty(this.props) === false) {
+      const { latitude, longitude } = this.props.location
+      let data = await axios.get(
+        `${baseUrl}?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+      )
+      console.log(data)
+    }
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit = async e => {
+    if (e.key === 'Enter') {
+      const { city } = this.state;
+      let data = await axios.get(
+        `${baseUrl}?q=${city}&APPID=${API_KEY}`
+      )
+      console.log(data)
+    }
   }
 
   render() {
     return (
       <div className="ui stackable two column grid">
         <div className="column">
-          <div style={{marginTop: '10px', marginLeft: '10px'}} className="ui segment">
+          <div
+            style={{ marginTop: '10px', marginLeft: '10px' }}
+            className="ui segment"
+          >
             <div className="ui center aligned header">
               Current Weather and Forecast for Your Location
             </div>
           </div>
           {_.isEmpty(this.props) ? (
-            <span style={{paddingLeft: '20px', color: 'red'}}>Please enable your location in order to see the current conditions and forecast for your area!</span>
+            <span style={{ paddingLeft: '20px', color: 'red' }}>
+              Please enable your location in order to see the current conditions
+              and forecast for your area!
+            </span>
           ) : (
-            <div style={{textAlign: 'center'}}>Here's your info!</div>
+            <div style={{ textAlign: 'center' }}>Here's your info!</div>
           )}
         </div>
         <div className="column">
-          <div style={{marginTop: '5px', marginRight: '10px'}} className="ui segment">
-            <div className='ui fluid input'>
-                <input type='text' placeholder='Enter city name...'/>
+          <div
+            style={{ marginTop: '5px', marginRight: '10px' }}
+            className="ui segment"
+          >
+            <div className="ui fluid input">
+              <input
+                onKeyPress={this.onSubmit}
+                onChange={this.onChange}
+                name="city"
+                type="text"
+                placeholder="Enter city name..."
+              />
             </div>
           </div>
         </div>
