8b1b9659004eee609c8226769267e49bb888fe84
Hieu Tran <hmtran.ae@gmail.com>
2019-01-29 21:02:09 -0500

    added https in front of baseUrl

--- src/apis/OpenWeatherMap.js
+++ src/apis/OpenWeatherMap.js
@@ -1,2 +1,2 @@
-export const API_KEY = 'd5bdbf038c6ec222514bb017ed0c743a';
-export const baseURL = 'api.openweathermap.org/data/2.5/'
+export const API_KEY = 'd5bdbf038c6ec222514bb017ed0c743a'
+export const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
