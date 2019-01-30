46110413165291c9297ad5d21fd72579b7d36d74
Hieu Tran <hmtran.ae@gmail.com>
2019-01-29 19:50:28 -0500

    locationinput received props of userLocation correctly

---
+++ src/components/LocationInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class LocationInput extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        console.log(this.props)
+        return <div>LocationInput</div>
+    }
+}
