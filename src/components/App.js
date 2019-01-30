002f1b4564cf7cc9cb97f88ecc5ef87de8327ce3
Hieu Tran <hmtran.ae@gmail.com>
2019-01-29 19:47:30 -0500

    send down props to locationinput component depending on permissions of user

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,38 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
+import LocationInput from './LocationInput'
 
 export default class App extends Component {
-    state = {
-        userLocation: {
-            latitude: null,
-            longitude: null
-        }
+  state = {
+    userLocation: {
+      latitude: null,
+      longitude: null
     }
+  }
 
-    componentDidMount() {
-        navigator.geolocation.getCurrentPosition(position => {
-            const { latitude, longitude } = position.coords
-            const userLocation = {
-                latitude: latitude,
-                longitude: longitude
-            }
-            this.setState({
-                userLocation
-            })
-        })
-    }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords
+      const userLocation = {
+        latitude: latitude,
+        longitude: longitude
+      }
+      this.setState({
+        userLocation
+      })
+    })
+  }
 
-    render() {
-        console.log(this.state.userLocation)
-        return (
-            <div>App</div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        App
+        {this.state.userLocation.latitude ? (
+          <LocationInput location={this.state.location} />
+        ) : (
+          <LocationInput />
+        )}
+      </div>
+    )
+  }
 }
