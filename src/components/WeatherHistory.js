5c15e61ef6f497d5f3ec71172356924933d679a4
Hieu Tran <hmtran.ae@gmail.com>
2019-01-31 09:45:11 -0500

    successfully grab stateId from value in state dropdown using get request to states endpoint

--- src/components/WeatherHistory.js
+++ src/components/WeatherHistory.js
@@ -1,61 +1,68 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import Chart from 'react-apexcharts'
-import { token } from '../apis/NOAA'
-import { Dropdown } from 'semantic-ui-react'
-import { stateOptions } from '../utils/StateOptions'
-import { yearOptions } from '../utils/YearOptions'
+import React, { Component } from "react";
+import axios from "axios";
+import Chart from "react-apexcharts";
+import { token } from "../apis/NOAA";
+import { Dropdown } from "semantic-ui-react";
+import { stateOptions } from "../utils/StateOptions";
+import { yearOptions } from "../utils/YearOptions";
+
+const config = {
+  headers: { token: token }
+};
 
 export default class WeatherHistory extends Component {
   state = {
-    yearValue: '',
-    stateValue: ''
-  }
+    yearValue: "",
+    stateValue: "",
+    stateId: ''
+  };
 
   componentDidMount = async () => {
-    const config = {
-      headers: { token: token }
-    }
-    const states = await axios.get(
-      'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=ST&limit=52',
-      config
-    )
-    console.log(states)
     const data = await axios.get(
-      'https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets',
+      "https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets",
       config
-    )
-    console.log(data)
-  }
+    );
+    // console.log(data);
+  };
 
   onYearChange = (e, { value }) => {
     this.setState({
       yearValue: value
-    })
-  }
+    });
+  };
 
-  onStateChange = (e, { value }) => {
+  onStateChange = async (e, { value }) => {
+    const stateData = await axios.get(
+      "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?locationcategoryid=ST&limit=52",
+      config
+    );
+    const stateId = stateData.data.results.filter(state => state.name === value)
     this.setState({
-      stateValue: value
-    })
-  }
+      stateValue: value,
+      stateId: stateId[0].id
+    });
+  };
 
   render() {
-    console.log(this.state.yearValue)
-    console.log(this.state.stateValue)
+    console.log(this.state.stateId)
     return (
       <div>
         <div className="ui divider" />
         <div className="ui one column centered grid">
           <div className="column">
-            <div style={{paddingTop: '20px'}} className='ui center aligned large header'>See how the weather has changed over time!</div>
             <div
-              style={{ paddingTop: '50px', paddingBottom: '150px' }}
+              style={{ paddingTop: "20px" }}
+              className="ui center aligned large header"
+            >
+              See how the weather has changed over time!
+            </div>
+            <div
+              style={{ paddingTop: "50px", paddingBottom: "150px" }}
               className="ui text container"
             >
               <Dropdown
-                style={{marginBottom: '20px'}}
-                placeholder='Data dating back...'
+                style={{ marginBottom: "20px" }}
+                placeholder="Data dating back..."
                 search
                 fluid
                 selection
@@ -76,6 +83,6 @@ export default class WeatherHistory extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
