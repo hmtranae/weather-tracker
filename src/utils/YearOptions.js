5f0bf727320b473a0ddc16b950ee12dd7190f2cb
Hieu Tran <hmtran.ae@gmail.com>
2019-01-30 21:51:35 -0500

    export dropdown options into util folder for states and years history

---
+++ src/utils/YearOptions.js
@@ -0,0 +1,9 @@
+export const yearOptions = [
+    {key: '1', value: '1', text: 'One year'},
+    {key: '2', value: '2', text: 'Two years'},
+    {key: '3', value: '3', text: 'Three years'},
+    {key: '4', value: '4', text: 'Four years'},
+    {key: '5', value: '5', text: 'Five years'},
+    {key: '10', value: '10', text: 'Ten years'},
+    {key: '15', value: '15', text: 'Fifteen years'},
+]
